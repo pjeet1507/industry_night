8eaaa04f9af8c794de164f4f89857b407ac3abe2
pjeet1507 <122104759+pjeet1507@users.noreply.github.com>
2024-04-07 23:57:02 -0400

    scroll changes

--- js/main.js
+++ js/main.js
@@ -5,13 +5,36 @@ import { updateProgressBar } from "./modules/progessbar.js";
 import { navgsap } from "./modules/hambur.js";
 import { leftandright } from "./modules/gsap.js";
 import { DomainLinkFix } from "./modules/DomainLinkFix.js";
+import { pagegsap } from "./modules/pagegsap.js";
 
+pagegsap();
 portfolios();
 accordian();
 hambur();
 navgsap();
-leftandright();
+// leftandright();
 DomainLinkFix();
+
 window.onscroll = function () {
   updateProgressBar();
 };
+
+window.addEventListener("scroll", function () {
+  // Get all sections
+  const sections = document.querySelectorAll("section");
+
+  // Iterate through each section
+  sections.forEach((section) => {
+    // Calculate the position of the section relative to the viewport
+    const rect = section.getBoundingClientRect();
+
+    // If the section touches the top of the viewport
+    if (rect.top <= 0 && rect.bottom >= 0) {
+      // Add a class to indicate the active section
+      section.classList.add("active");
+    } else {
+      // Remove the class if the section is not in the viewport
+      section.classList.remove("active");
+    }
+  });
+});
